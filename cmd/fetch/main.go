9d0bc1559ea261373246f2c53e60be1a6d96b6ec
Carter Wobbe <wobbe.carter@gmail.com>
2025-08-10 02:53:55 -0700

    Add basic fetch cli

---
+++ cmd/fetch/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"mtgBuilder/fetch"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("please supply exactly 1 argument")
+	}
+
+	f, err := os.OpenFile(os.Args[1], os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	js, err := fetch.GetOracleCardsJSON()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = f.Write(js)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
